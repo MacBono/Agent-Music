215162dd9db3c2248c090e431cff594ffc1d2cbf
MacBono <60624796+MacBono@users.noreply.github.com>
2022-01-09 22:18:20 +0100

    Update melodyRule.py

--- melodyRule.py
+++ melodyRule.py
@@ -1,18 +1,19 @@
-from cell import *
 import random
+from cell import *
+from rule import *
 
 class MelodyRule():
 	def __init__(self, root_key, scale, octave):
 		self.keys_list = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 		self.octave = int(octave)
-		self.root_key = MelodyRule.getRoot(root_key, self.octave, self.keys_list)
+		self.root_key = MelodyRule.getRoot(root_key,self.octave,self.keys_list)
 		self.scale = MelodyRule.getScale(scale)
 		self.my_notes = []
-	
-	def getRoot(name,octave, keys_list):
-		root= 60 + 12*octave + keys_list.index(name)
-		return root  
-	
+
+	def getRoot(name, octave, keys_list):
+		root = 60 + 12*octave + keys_list.index(name)
+		return root
+
 	def getScale(name):
 		if name=='Major':
 			scale_rule=[0,2,4,5,7,9,11]
@@ -22,7 +23,7 @@ class MelodyRule():
 			scale_rule=[]
 		return scale_rule
 	
-	def getKey(key, octave, scale, pos):
+	def getKey(key,octave,scale,pos):
 		tmp_octave = octave
 		tmp_pos = pos
 		if pos >= len(scale):
@@ -32,21 +33,21 @@ class MelodyRule():
 			tmp_octave = tmp_octave - 1
 			tmp_pos = tmp_pos % len(scale)
 		return key + 12*tmp_octave + scale[tmp_pos]
-		
+
 	def evaluate(self, notes, current_index):
 		self.my_notes = []
 		# First chord root in octave
 		if current_index == 0 or len(notes) == 0:
 			rnd = random.randint(0,6)
 			for i in range(rnd, rnd + 6, 2):
-				self.my_notes.append(Cell(MelodyRule.getKey(self.root_key,self.octave, self.scale, i)))
+				self.my_notes.append(Cell(MelodyRule.getKey(self.root_key,self.octave,self.scale,i)))
 			return self.my_notes
 		else:
 			rnd = random.randint(-2, 6)
-			previous = notes[current_index - 1]
+			previous = notes[current_index-1]
 			previous_root = previous[0].pitch
-			while MelodyRule.getKey(self.root_key,self.octave, self.scale, rnd) == previous_root:
-				rnd = random.randint(-2, 6)
-			for i in range(rnd, rnd + 3, 2):
-				self.my_notes.append(Cell(MelodyRule.getKey(self.root_key,self.octave, self.scale, i)))
+			while MelodyRule.getKey(self.root_key,self.octave,self.scale,rnd) == previous_root:
+				rnd = random.randint(0, 6)
+			for i in range(rnd, rnd + 6, 2):
+				self.my_notes.append(Cell(MelodyRule.getKey(self.root_key,self.octave,self.scale,i)))
 			return self.my_notes
